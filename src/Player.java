7d08e072813dad75ae97e84ba0184f1622a0c3b0
Naatiq@MacBook Air <naatiq@gmail.com>
2014-07-13 14:33:25 +0530

    Completed code,

---
+++ src/Player.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+/**
+ * Created by naatiqullahmohammed on 07/07/14.
+ */
+public class Player {
+    private boolean isStanding;
+    private ArrayList<Tile> position = new ArrayList<Tile>(2);
+}
---
+++ src/Tile.java
@@ -0,0 +1,12 @@
+/**
+ * Created by naatiqullahmohammed on 07/07/14.
+ */
+public class Tile {
+    private int xPos = 0;
+    private int yPos = 0;
+
+    public Tile(int xPos, int yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
